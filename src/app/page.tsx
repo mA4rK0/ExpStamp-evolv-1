92584616bc5da892c4db52c2c652828601e65aa2
marko <onlymarko25@gmail.com>
2025-05-06 09:45:21 +0700

    feat: add route navigation logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,46 @@
+"use client";
+import { Stamp } from "lucide-react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useConnectModal, useActiveAccount } from "thirdweb/react";
+import client from "@/api/key/connect";
+
 export default function Home() {
-  return <div></div>;
+  const { connect, isConnecting } = useConnectModal();
+  const account = useActiveAccount();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (account?.address && !isConnecting) {
+      router.push("/mint");
+    }
+  }, [account]);
+
+  async function handleConnect() {
+    try {
+      const wallet = await connect({ client });
+      const account = wallet.getAccount();
+      await account?.signMessage({
+        message: "welcome to ExpStamp, you can mint your NFTs here. Currently, only Ethereum Sepolia is supported",
+      });
+
+      console.log("connected to", account);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="flex flex-wrap min-h-screen w-full bg-black">
+      <div>
+        <Stamp className="text-indigo-500" />
+      </div>
+      <div>
+        <p className="text-white">Mint your NFTs now on ExpStamp!</p>
+        <button onClick={handleConnect} className="text-white">
+          Connect
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,28 @@
+"use client";
 import Link from "next/link";
-import { ConnectButton } from "thirdweb/react";
+import { useEffect, useState } from "react";
+import { ConnectButton, useActiveAccount } from "thirdweb/react";
 import { sepolia, arbitrumSepolia, arbitrum } from "thirdweb/chains";
 import client from "@/api/key/connect";
 
 const Navbar: React.FC = () => {
+  const [hydration, setHydration] = useState(false);
+  const account = useActiveAccount();
+  const isDisabled = !!account;
+
+  useEffect(() => {
+    setHydration(true);
+  }, []);
+
   return (
     <div className="flex flex-wrap w-full px-7 py-3 bg-[#212121] text-white items-center justify-between">
       <div>
-        <Link href={"/"}>
+        <Link
+          href={"/"}
+          onClick={(e) => {
+            if (isDisabled) e.preventDefault();
+          }}
+        >
           <h1 className="text-xl font-bold">ExpStamp</h1>
         </Link>
       </div>
@@ -19,21 +34,23 @@ const Navbar: React.FC = () => {
         ))}
       </div>
       <div>
-        <ConnectButton
-          client={client}
-          theme="dark"
-          connectButton={{
-            label: "Sign In",
-            style: {
-              color: "black",
-              background: "white",
-              width: "141px",
-              height: "34px",
-              fontSize: "15px",
-            },
-          }}
-          chains={[sepolia, arbitrumSepolia, arbitrum]}
-        />
+        {hydration && (
+          <ConnectButton
+            client={client}
+            theme="dark"
+            connectButton={{
+              label: "Sign In",
+              style: {
+                color: "black",
+                background: "white",
+                width: "141px",
+                height: "34px",
+                fontSize: "15px",
+              },
+            }}
+            chains={[sepolia, arbitrumSepolia, arbitrum]}
+          />
+        )}
       </div>
     </div>
   );
